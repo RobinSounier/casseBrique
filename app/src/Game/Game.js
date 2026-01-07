6cbd0837ffec6c8dbd17fde167b29e1d8e7cb3d5
RobinSounier <sounierrobin@gmail.com>
2026-01-07 12:15:26 +0100

    midi

--- app/src/Game/Game.js
+++ app/src/Game/Game.js
@@ -1,41 +1,53 @@
-import '../assets/css/style.css'
+// Import de la feuille de style
+import '../assets/css/style.css';
+//import des assets de sprites
+import ballIMGSRC from '../assets/img/ball.png';
 
 class Game
 {
-
-    //Contexte de sessin du canvas
+    // Contexte de dessin du canvas
     ctx;
 
-    start(){
-        console.log("Game start");
-        this.initHtmlUI()
+    start() {
+        console.log('Jeu démarré ...');
+        this.initHtmlUI();
+
+        //1- on crée une balise html img qui ne sera jamais ajouter au dom
+        const ballImg = new Image();
+
+        // 2- on récupère le nom de l'imlage généré par webpack en tant que src de cette image
+        ballImg.src = ballIMGSRC;
+
+        // "- On demande au contexte de dessin de dessiner cette image dans le canvas
+        ballImg.addEventListener('load', () => {
+            this.ctx.drawImage(ballImg, 400, 300);
+        })
+
+
     }
 
-    // méthode privée
-    initHtmlUI()
-    {
-        const elH1 = document.createElement('h1')
-        elH1.textContent= 'Casse brique'
+    // Méthodes "privées"
+    initHtmlUI() {
+        const elH1 = document.createElement('h1');
+        elH1.textContent = 'Arkanoïd';
 
-        const elCanvas = document.createElement('canvas')
-
+        const elCanvas = document.createElement( 'canvas' );
         elCanvas.width = 800;
         elCanvas.height = 600;
 
         document.body.append( elH1, elCanvas );
 
-        //Récuperation du contexte du canvas
-        this.ctx = elCanvas.getContext('2d')
+        // Récupération du contexte de dessin
+        this.ctx = elCanvas.getContext('2d');
     }
 
-    //fonction test inutile en jeux
-
+    // Fonction de test inutile dans le jeu
     drawTest() {
         this.ctx.beginPath();
-        this.ctx.fillStyle = '#ffdd00';
-        this.ctx.arc(400, 300, 100, Math.PI * 2 ,Math.PI * 2)
-        this.ctx.fill()
-        this.ctx.closePath()
+        this.ctx.fillStyle = '#fc0';
+        this.ctx.arc(400, 300, 100, 0, Math.PI * 2 - Math.PI / 3);
+        this.ctx.closePath();
+        this.ctx.fill();
     }
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -2,4 +2,3 @@ import theGame from "./Game/Game";
 
 theGame.start();
 
-theGame.drawTest();
