252f1d90c8f1f4a496c05515b584cda23ce7ac8e
RobinSounier <sounierrobin@gmail.com>
2026-01-07 14:37:31 +0100

    the ball is on mouvement but exit the screen

--- app/src/Game/Game.js
+++ app/src/Game/Game.js
@@ -1,33 +1,25 @@
 // Import de la feuille de style
 import '../assets/css/style.css';
 //import des assets de sprites
-import ballIMGSRC from '../assets/img/ball.png';
+import ballImgSrc from '../assets/img/ball.png';
 
 class Game
 {
     // Contexte de dessin du canvas
     ctx;
 
+    //temporaire: position de base de la balle
+    ballX = 400;
+    ballY= 300;
+
     start() {
         console.log('Jeu démarré ...');
         this.initHtmlUI();
-
-        //1- on crée une balise html img qui ne sera jamais ajouter au dom
-        const ballImg = new Image();
-
-        // 2- on récupère le nom de l'imlage généré par webpack en tant que src de cette image
-        ballImg.src = ballIMGSRC;
-
-        // "- On demande au contexte de dessin de dessiner cette image dans le canvas
-        ballImg.addEventListener('load', () => {
-            this.ctx.drawImage(ballImg, 400, 300);
-        })
-
-
+        requestAnimationFrame(this.loop.bind(this));
     }
 
     // Méthodes "privées"
-    initHtmlUI() {
+    initHtmlUI(){
         const elH1 = document.createElement('h1');
         elH1.textContent = 'Arkanoïd';
 
@@ -39,6 +31,32 @@ class Game
 
         // Récupération du contexte de dessin
         this.ctx = elCanvas.getContext('2d');
+    }
+
+    //boucle d'animation
+    loop()
+    {
+
+        // 1- on crée une balise HTML <img> qui ne sera jamais ajoutée au DOM
+        const ballImg = new Image();
+
+
+        // 2- On récupére le nom de l'image généré par webpack en tant que src de cette image
+        ballImg.src = ballImgSrc;
+
+
+        // 3- On demande au contexte de dessin de dessiner cette image dans le canvas
+        ballImg.addEventListener( 'load', () => {
+            this.ctx.drawImage( ballImg, this.ballX, this.ballY );
+        });
+
+
+        // Mise a jour de la position de la balle
+        this.ballX ++
+        this.ballY --
+
+        //appelle de la frame suivantes
+        requestAnimationFrame(this.loop.bind(this));
     }
 
     // Fonction de test inutile dans le jeu
